087b45ea64f07b50e47364f376cdad5776cb261e
Kannan <kannangod123@gmail.com>
2025-09-30 14:48:15 +0530

    Fix path resolution issue - change moduleResolution to node and add webpack alias

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
